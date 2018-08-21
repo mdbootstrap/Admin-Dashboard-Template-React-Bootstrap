e39dc3e607b8529b1bb6ccbcdccf7cabffcfd1a7
anna-morawska <a.morawska@mdbootstrap.com>
2018-08-21 09:10:27 +0200

    Remove waves from Prifile Page

--- src/components/pages/ProfilePage.js
+++ src/components/pages/ProfilePage.js
@@ -51,7 +51,7 @@ const ProfilePage =  () => {
               <Card className="d-flex mb-5">
                 <View>
                   <img src="https://mdbootstrap.com/img/Mockups/Horizontal/6-col/pro-signup.jpg" alt="Project" className="img-fluid"/>
-                  <Mask overlay="white-slight" waves/>
+                  <Mask overlay="white-slight"/>
                 </View>
                 <CardBody>
                   <CardTitle className="font-bold mb-3">
@@ -75,7 +75,7 @@ const ProfilePage =  () => {
               <Card className="d-flex mb-5">
                 <View>
                   <img src="https://mdbootstrap.com/img/Mockups/Horizontal/6-col/pro-profile-page.jpg" alt="Project" className="img-fluid"/>
-                  <Mask overlay="white-slight" waves/>
+                  <Mask overlay="white-slight"/>
                 </View>
                 <CardBody>
                   <CardTitle className="font-bold mb-3">
@@ -97,7 +97,7 @@ const ProfilePage =  () => {
               <Card className="d-flex mb-5">
                 <view-wrapper>
                   <img src="https://mdbootstrap.com/img/Mockups/Horizontal/6-col/pro-signup.jpg" alt="Project" className="img-fluid"/>
-                  <Mask overlay="white-slight" waves/>
+                  <Mask overlay="white-slight"/>
                 </view-wrapper>
                 <CardBody>
                   <CardTitle className="font-bold mb-3">
